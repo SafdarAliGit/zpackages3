e5f5b81f4b7ca12705179f2fb67c2944932e341f
SafdarAliGit <safdar211@gmail.com>
2026-04-22 16:15:28 +0500

    updated

---
+++ zpackages3/zpackages3/doctype/compliance_attendance/compliance_attendance.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Safdar Ali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Compliance Attendance", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ zpackages3/zpackages3/doctype/compliance_attendance/test_compliance_attendance.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Safdar Ali and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestComplianceAttendance(FrappeTestCase):
+	pass
